6a8c474993826b24a8a9669e476a88e53e44bc15
Tom Wolverson <t_wolverson@hotmail.com>
2017-06-04 16:35:05 +0100

    more complex network example

--- Traffic.Tests/UnitTest1.cs
+++ Traffic.Tests/UnitTest1.cs
@@ -26,6 +26,64 @@ namespace Traffic.Tests
                 Links = links
             };
 
+            var controllers = new Controller[1];
+            var phases = new Phase[5]
+            {
+                        new Phase(),
+                        new Phase(),
+                        new Phase(),
+                        new Phase(),
+                        new Phase()
+                        };
+
+
+            phases[0].ConflictsWithPhases = new Phase[]{
+                phases[2],
+                phases[3],
+                phases[4]
+            };
+            phases[1].ConflictsWithPhases = new Phase[]{
+                phases[2],
+                phases[3],
+                phases[4]
+            };
+
+            phases[2].ConflictsWithPhases = new Phase[]{
+                phases[0],
+                phases[1],
+                phases[3],
+                phases[4]
+            };
+
+            phases[3].ConflictsWithPhases = new Phase[]{
+                phases[0],
+                phases[1],
+                phases[2]
+            };
+
+            phases[4].ConflictsWithPhases = new Phase[]{
+                phases[0],
+                phases[1],
+                phases[2]
+            };
+
+            var stages = new Stage[3] { new Stage { StartTime = 0, EndTime = 30 }, new Stage { StartTime = 35, EndTime = 60 }, new Stage { StartTime = 65, EndTime = 90 } };
+
+            phases[0].RunsInStages = new[] { stages[0] };
+            phases[1].RunsInStages = new[] { stages[0] };
+            phases[2].RunsInStages = new[] { stages[1] };
+            phases[3].RunsInStages = new[] { stages[2] };
+            phases[4].RunsInStages = new[] { stages[2] };
+
+            controllers[0] = new Controller
+            {
+                Phases = phases,
+                Stages = stages,
+                CycleTime = 95
+            };
+
+            network.Controllers = controllers;
+            
             var str = JsonConvert.SerializeObject(network, new JsonSerializerSettings { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
             Console.Write(str);
             File.WriteAllText("newnetwork.json", str);
